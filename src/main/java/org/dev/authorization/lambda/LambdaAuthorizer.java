8403880880ca17ca54422f74f94fd7d1bb1b1599
Shivam Tyagi <shivamtyagicool@gmail.com>
2023-03-09 09:30:33 +0530

    removing unwanted code

--- src/main/java/org/dev/authorization/lambda/LambdaAuthorizer.java
+++ src/main/java/org/dev/authorization/lambda/LambdaAuthorizer.java
@@ -1,44 +1,29 @@
 package org.dev.authorization.lambda;
 
-import com.amazonaws.HttpMethod;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+
 public class LambdaAuthorizer implements RequestHandler<Request, Response> {
     public Response handleRequest(Request event, Context context) {
 
 
-//        try {
-//
-//            System.out.println(new ObjectMapper().writeValueAsString(event1));
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
-//        APIGatewayProxyRequestEvent event =(APIGatewayProxyRequestEvent) event1;
-//        Map<String, String> headers = event.getAuthorizationToken();
+        try {
+            System.out.println(new ObjectMapper().writeValueAsString(event));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         String authorizationToken = event.getAuthorizationToken();
 
         String auth = "Deny";
-        System.out.println("=====>>>"+authorizationToken);
         String sub = JWTUtil.getSub(authorizationToken);
         if (sub != null) {
             auth = "Allow";
         }
-//        Map<String, String> ctx = new HashMap<String, String>();
-//        ctx.put("sub", sub);
-//        For Http purpose
-//        APIGatewayProxyRequestEvent.ProxyRequestContext proxyContext = event.getRequestContext();
-//        System.out.println(event.getRequestContext());
-//        System.out.println("proxyContext=====>>>"+proxyContext);
-//        APIGatewayProxyRequestEvent.RequestIdentity identity = proxyContext.getIdentity();
-//         System.out.println("identity=====>>>"+identity);
 
         String methodArn = event.getMethodArn();
 
@@ -55,10 +40,10 @@ public class LambdaAuthorizer implements RequestHandler<Request, Response> {
         }
 
 
-        System.out.println("Region == "+region+" , awsAccountId == "+awsAccountId+"restApiId == "+restApiId+" , stage == "+stage+" , httpMethod= "+httpMethod+" , resource="+resource);
+        System.out.println("Region == " + region + " , awsAccountId == " + awsAccountId + "restApiId == " + restApiId + " , stage == " + stage + " , httpMethod= " + httpMethod + " , resource=" + resource);
 
-        String arn = String.format("arn:aws:execute-api:%s:%s:%s/%s/%s/%s",System.getenv("AWS_REGION"), awsAccountId,
-                restApiId, stage,httpMethod, "*");
+        String arn = String.format("arn:aws:execute-api:%s:%s:%s/%s/%s/%s", System.getenv("AWS_REGION"), awsAccountId,
+                restApiId, stage, httpMethod, "*");
         Statement statement = Statement.builder().effect(auth).resource(arn).build();
 
         PolicyDocument policyDocument = PolicyDocument.builder().statements(Collections.singletonList(statement))
